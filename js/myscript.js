07fd9fa38c2e98a41f2018baf2e484e0e58674b2
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-16 16:09:47 +0200

    esercizio terminato

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,10 @@
 for (let i = 0; i < 100; i = i + 1) {
-    if (i % 3 === 0 && i >= 3) {
-        console.log('Fizz');
+    if (i % 15 === 0 && i >= 15) {
+        console.log('FizzBuzz');
     } else if (i % 5 === 0 && i >= 5){
         console.log('Buzz');
-    } else if ((i % 5 === 0 && i >= 5) && (i % 3 === 0 && i >= 3)){
-        console.log('FizzBuzz');
+    } else if (i % 3 === 0 && i >= 3){
+        console.log('Fizz');
     } else {
         console.log(i);
     }
